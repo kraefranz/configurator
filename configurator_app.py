baa76cd2a040724514166fd89d036b774d761149
Franz <kraefranz@posteo.de>
2026-01-20 19:39:07 +0100

    Changed SSO text

--- configurator_app.py
+++ configurator_app.py
@@ -75,7 +75,7 @@ with st.container(border=True):
 
     st.markdown(
     "<div style='font-size:1.1rem; font-weight:600;'>"
-    "Login über das Single-Sign-On (SSO) Ihrer Organisation"
+    "Login über Single-Sign-On (SSO) Ihrer Organisation"
     "</div>",
     unsafe_allow_html=True
     )
